50ee1041ef723048a3083448483ab8d3991f7ec4
813hoyeon <pphy08133@hufs.ac.kr>
2025-07-15 23:00:20 +0900

    Title: 합과 평균, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 250715/합과 평균/sum-and-avg.cpp
+++ 250715/합과 평균/sum-and-avg.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
-#include <iomanip>
 
 using namespace std;
 
 int main() {
     int a, b;
-    cin >> a >> b;
-    cout << a + b << " ";
-    cout << fixed << setprecision(1) << (double)(a + b) / 2;
+
+	cin >> a >> b;
+	
+	cout << fixed;
+	cout.precision(1);
+    
+    cout << a + b << " " << (double)(a + b) / 2;
     return 0;
 }
