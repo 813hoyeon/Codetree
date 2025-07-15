bcf3ff560ff42a517784685570d3e72cc63b97f3
813hoyeon <pphy08133@hufs.ac.kr>
2025-07-15 21:23:45 +0900

    Title: 전화번호 바꾸기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250715/전화번호 바꾸기/change-phone-number.cpp
+++ 250715/전화번호 바꾸기/change-phone-number.cpp
@@ -2,6 +2,9 @@
 using namespace std;
 
 int main() {
-    // Please write your code here.
+    int a,b,c;
+    char d;
+    cin >> a >> d >> b >> d >> c;
+    cout << "010" << d << c << d << b;
     return 0;
 }
