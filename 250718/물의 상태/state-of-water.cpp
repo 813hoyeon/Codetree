2cdca015093cacb5de31bb4c3e9b7cd1f7f3ef10
813hoyeon <pphy08133@hufs.ac.kr>
2025-07-18 21:15:08 +0900

    Title: 물의 상태, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250718/물의 상태/state-of-water.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int w;
+    cin >> w;
+    if (w < 0) {
+        cout << "ice";
+    }
+    else if (w >= 100) {
+        cout << "vapor";
+    }
+    else {
+        cout << "water";
+    }
+    return 0;
+}
