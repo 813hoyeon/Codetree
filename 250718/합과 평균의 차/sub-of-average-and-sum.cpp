50f43d6117cb112f71cb9197e81276ae5cbfa377
813hoyeon <pphy08133@hufs.ac.kr>
2025-07-18 20:01:14 +0900

    Title: 합과 평균의 차, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250718/합과 평균의 차/sub-of-average-and-sum.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b,c;
+    cin >> a >> b >> c;
+    int d,e;
+    d = a + b + c;
+    e = (a + b + c) / 3;
+    cout << d << endl << e << endl << d - e;
+    return 0;
+}
