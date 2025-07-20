272f8ff3ca5535f2dbde483a7ec372b0696af350
813hoyeon <pphy08133@hufs.ac.kr>
2025-07-20 20:31:21 +0900

    Title: 비교 연산, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250720/비교 연산/comparison-operator.cpp
+++ 250720/비교 연산/comparison-operator.cpp
@@ -4,6 +4,6 @@ using namespace std;
 int main() {
     int A,B;
     cin >> A >> B;
-    cout << (A >= B) << endl << (A > B) << endl << (A <= B) << endl << (A < B) << endl << (A == B) << (A != B);
+    cout << (A >= B) << endl << (A > B) << endl << (A <= B) << endl << (A < B) << endl << (A == B) << endl << (A != B);
     return 0;
 }
