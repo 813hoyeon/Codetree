184b5c8b79615effbd4a9056468d3aa4d0a07b8a
813hoyeon <pphy08133@hufs.ac.kr>
2025-07-20 20:44:36 +0900

    Title: 범위 안의 수, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250720/범위 안의 수/number-in-range.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A;
+    cin >> A;
+    if (A >= 10 && A <= 20) {
+        cout << "yes";
+    }
+    else {
+        cout << "no";
+    }
+    return 0;
+}
