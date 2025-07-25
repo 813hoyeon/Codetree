3ae22a5e19f5da53fa376ccc6b27ae5fedb1b55d
813hoyeon <pphy08133@hufs.ac.kr>
2025-07-26 07:17:53 +0900

    Title: 짝수만 출력, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250726/짝수만 출력/output-only-even.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A,B;
+    cin >> A >> B;
+    int i = A;
+    while (i <= B) {
+        if (i % 2 == 0) {
+            cout << i << " ";
+        }
+        i += 1;
+    }
+    return 0;
+}
