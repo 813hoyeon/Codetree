8630f4c409666c21bb46f9d689cd79a2e2bf8837
813hoyeon <pphy08133@hufs.ac.kr>
2025-07-27 20:33:27 +0900

    Title: 홀수만 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 250727/홀수만 출력하기/print-only-odd-numbers.cpp
+++ 250727/홀수만 출력하기/print-only-odd-numbers.cpp
@@ -7,7 +7,7 @@ int main() {
     for (int i = 1; i <= N; i++) {
         int k;
         cin >> k;
-        if (k % 3 == 0) {
+        if ((k % 3 == 0) && (k % 2 == 1)) {
             cout << k << endl;
         }
     }
