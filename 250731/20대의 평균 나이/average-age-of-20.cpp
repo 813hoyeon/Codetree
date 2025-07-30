9dcb2fe9d9e78bcc0ac6249b30252d100643fb8a
813hoyeon <pphy08133@hufs.ac.kr>
2025-07-31 01:53:43 +0900

    Title: 20대의 평균 나이, Time: 1072ms, Memory: 0MB, Status: Time Limit Exceed - Codetree

---
+++ 250731/20대의 평균 나이/average-age-of-20.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int sum = 0;
+    int cnt = 0;
+    while (1) {
+        int age;
+        cin >> age;
+        if (age >= 30) {
+            cout << fixed;
+            cout.precision(2);
+            cout << (double)sum / cnt;
+            break;
+        }
+        sum += age;
+        cnt++;
+    }
+    return 0;
+}
