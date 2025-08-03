af99884764de9e525e597f83fdf2537e56b121ec
813hoyeon <pphy08133@hufs.ac.kr>
2025-08-04 03:33:40 +0900

    Title: ab 사이에 없는 c, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250804/ab 사이에 없는 c/c-between-a-and-b-2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b,c;
+    cin >> a >> b >> c;
+    bool satisfied = false;
+    for (int i = a; i <= b; i++) {
+        if (i % c == 0) {
+            satisfied = true;
+            break;
+        }
+        else {
+
+        }
+        
+    }
+    if (satisfied == true) {
+        cout << "NO";
+    }
+    else {
+        cout << "YES";
+    }
+    return 0;
+}
