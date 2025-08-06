673f06c1505a99405bf5d43f293261f9d2842442
813hoyeon <pphy08133@hufs.ac.kr>
2025-08-06 20:57:58 +0900

    Title: 정사각형 출력, Time: 9ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250806/정사각형 출력/print-square.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    int cnt = 1;
+    for (int i = 1; i <= N; i++) {
+        for (int j = 1; j <= N; j++) {
+            cout << cnt << " ";
+            cnt++;
+        }
+        cout << endl;
+    }
+    return 0;
+}
